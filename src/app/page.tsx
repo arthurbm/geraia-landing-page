c7e3d6c71c66dfb841cb8e3221b249c0570e4634
arthurbritom <arthurmedeiros32@gmail.com>
2023-12-17 09:02:20 -0300

    feat: make landing page with v0 and add it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,154 @@
-import Image from 'next/image'
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/Hfus9B9uNcM
+ */
+import { Button } from "@/components/ui/button"
+import { CardHeader, CardContent, Card } from "@/components/ui/card"
+import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar"
+import Image from "next/image"
 
-export default function Home() {
+export default function Component() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="w-full">
+      <section className="relative bg-[#ffffff] w-full">
+        <nav className="container mx-auto px-4 md:px-6 lg:px-8 py-4">
+          <div className="flex justify-between items-center">
+            <h1 className="text-2xl font-bold tracking-tighter">GERAIA</h1>
+            <div className="hidden lg:flex space-x-4">
+              <Button className="text-base text-black" variant="link">
+                Início
+              </Button>
+              <Button className="text-base text-black" variant="link">
+                Sobre
+              </Button>
+              <Button className="text-base text-black" variant="link">
+                Pesquisa
+              </Button>
+              <Button className="text-base text-black" variant="link">
+                Equipe
+              </Button>
+              <Button className="text-base text-black" variant="link">
+                Contato
+              </Button>
+            </div>
+          </div>
+        </nav>
+        <div className="container mx-auto px-4 md:px-6 lg:px-8 py-12 md:py-24 lg:py-32">
+          <h1 className="text-4xl font-bold tracking-tighter text-center mb-10">
+            Bem-vindo ao Grupo de Pesquisa GERAIA
+          </h1>
+          <p className="text-lg text-gray-600 text-center mb-10">Dedicado ao avanço da pesquisa em IA Generativa</p>
+          <div className="flex justify-center">
+            <Button className="mr-4" variant="link">
+              Saiba Mais
+            </Button>
+            <Button>Junte-se a nós</Button>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      </section>
+      <div className="container mx-auto px-4 md:px-6 lg:px-8 py-8">
+        <section className="mb-10">
+          <h2 className="text-3xl font-bold tracking-tighter mb-4">Nossas Áreas de Pesquisa</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <Card>
+              <CardHeader>
+                <h3 className="text-xl font-semibold">Modelos Generativos</h3>
+              </CardHeader>
+              <CardContent>
+                <p className="text-base text-gray-500">
+                  Nosso grupo está focado no desenvolvimento de novos modelos generativos para várias aplicações.
+                </p>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader>
+                <h3 className="text-xl font-semibold">Aprendizado por Reforço</h3>
+              </CardHeader>
+              <CardContent>
+                <p className="text-base text-gray-500">
+                  Estamos explorando como o aprendizado por reforço pode ser usado na IA generativa.
+                </p>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader>
+                <h3 className="text-xl font-semibold">Pesquisa de Arquitetura Neural</h3>
+              </CardHeader>
+              <CardContent>
+                <p className="text-base text-gray-500">
+                  Nossa pesquisa inclui a descoberta de arquiteturas de redes neurais ótimas para tarefas generativas.
+                </p>
+              </CardContent>
+            </Card>
+          </div>
+        </section>
+        <section className="mb-10">
+          <h2 className="text-3xl font-bold tracking-tighter mb-4">Sobre Nós</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+            <div>
+              <p className="text-lg text-gray-600 mb-4">
+                Somos um grupo de pesquisadores e engenheiros dedicados ao avanço do campo da IA Generativa. Nossa
+                paixão por IA nos levou a explorar vários aspectos de modelos generativos, aprendizado por reforço e
+                pesquisa de arquitetura neural.
+              </p>
+              <Button className="mt-4" variant="link">
+                Saiba Mais
+              </Button>
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <Image
+                alt="Imagem Sobre 1"
+                height="200"
+                src="/placeholder.svg"
+                style={{
+                  aspectRatio: "200/200",
+                  objectFit: "cover",
+                }}
+                width="200"
+              />
+              <Image
+                alt="Imagem Sobre 2"
+                height="200"
+                src="/placeholder.svg"
+                style={{
+                  aspectRatio: "200/200",
+                  objectFit: "cover",
+                }}
+                width="200"
+              />
+            </div>
+          </div>
+        </section>
+        <section className="mb-10">
+          <h2 className="text-3xl font-bold tracking-tighter mb-4">Nossa Equipe</h2>
+          <div className="flex items-center space-x-4">
+            <Avatar>
+              <AvatarImage alt="Membro da equipe 1" src="/placeholder.svg?height=200&width=200" />
+              <AvatarFallback>MT1</AvatarFallback>
+            </Avatar>
+            <Avatar>
+              <AvatarImage alt="Membro da equipe 2" src="/placeholder.svg?height=200&width=200" />
+              <AvatarFallback>MT2</AvatarFallback>
+            </Avatar>
+            <Avatar>
+              <AvatarImage alt="Membro da equipe 3" src="/placeholder.svg?height=200&width=200" />
+              <AvatarFallback>MT3</AvatarFallback>
+            </Avatar>
+            <Avatar>
+              <AvatarImage alt="Membro da equipe 4" src="/placeholder.svg?height=200&width=200" />
+              <AvatarFallback>MT4</AvatarFallback>
+            </Avatar>
+          </div>
+        </section>
+        <section>
+          <h2 className="text-3xl font-bold tracking-tighter mb-4">Entre em Contato</h2>
+          <p className="text-lg text-gray-600 mb-4">Interessado em nossa pesquisa? Entre em contato conosco.</p>
+          <Button className="w-full sm:w-auto">
+            Contate-nos
+          </Button>
+        </section>
       </div>
     </main>
   )
 }
+
